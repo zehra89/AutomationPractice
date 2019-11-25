ca53d810e64cb3576a99fdcba49329521b116d1d
zehra89 <zehra.mehak@simitive.com>
2019-11-25 10:27:14 +0000

    Extent report

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -1,6 +1,12 @@
 package runner;
 
+import java.io.File;
+
+import org.junit.AfterClass;
 import org.junit.runner.RunWith;
+
+import com.cucumber.listener.Reporter;
+
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
@@ -9,13 +15,19 @@ import cucumber.api.junit.Cucumber;
 @CucumberOptions(
 		
 		features = "classpath:features",
-		glue={"stepDefinitions"}
+		glue={"stepDefinitions"},
 		//tags = {"@First", "@Second"},
-		// plugin = { "com.cucumber.listener.ExtentCucumberFormatter:target/cucumber-reports/report.html"}
-		) 
+		plugin = { "com.cucumber.listener.ExtentCucumberFormatter:target/cucumber-reports/report.html"}, 
+		        monochrome = true
+		                )
+		
 
 public class TestRunner {
 	
+	 @AfterClass
+	    public static void writeExtentReport() {
+	        Reporter.loadXMLConfig(new File("config/report.xml"));
+	 }
 	
 	
 }
